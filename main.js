e85bfebd9377f19e05ed034421b1bcb501cb69de
William <williamdeividp@gmail.com>
2024-03-01 06:11:26 -0300

    formulário completo

--- main.js
+++ main.js
@@ -1,22 +1,30 @@
 import { generateReturnsArray } from "./src/investmentGoals.js";
 
 const form = document.getElementById("investment-form");
+const clearFormButton = document.getElementById("clear-form");
 //const calculateButton = document.getElementById("calculate-results");
 
 function renderProgression(e) {
   e.preventDefault();
+  if (document.querySelector(".error")) {
+    return;
+  }
   //const startingAmount = Number(form["starting-amount"].value);
   const startingAmount = Number(
-    document.getElementById("starting-amount").value
+    document.getElementById("starting-amount").value.replace(",", ".")
   );
   const additionalContribuition = Number(
-    document.getElementById("additional-contribuition").value
+    document.getElementById("additional-contribution").value.replace(",", ".")
   );
   const timeAmount = Number(document.getElementById("time-amount").value);
   const timeAmountPeriod = document.getElementById("time-amount-period").value;
-  const returnRate = Number(document.getElementById("return-rate").value);
+  const returnRate = Number(
+    document.getElementById("return-rate").value.replace(",", ".")
+  );
   const returnRatePeriod = document.getElementById("evaluation-period").value;
-  const taxRate = Number(document.getElementById("tax-rate").value);
+  const taxRate = Number(
+    document.getElementById("tax-rate").value.replace(",", ".")
+  );
 
   const returnsArray = generateReturnsArray(
     startingAmount,
@@ -29,5 +37,55 @@ function renderProgression(e) {
   console.log(returnsArray);
 }
 
+function clearForm() {
+  form["starting-amount"].value = "";
+  form["additional-contribution"].value = "";
+  form["time-amount"].value = "";
+  form["return-rate"].value = "";
+  form["tax-rate"].value = "";
+
+  const errorInputContainers = document.querySelectorAll(".error");
+  for (const errorInputContainer of errorInputContainers) {
+    errorInputContainer.classList.remove("error");
+    errorInputContainer.parentElement.querySelector("p").remove();
+  }
+}
+
+function validateInput(e) {
+  if (e.target.value === "") {
+    return;
+  }
+  const { parentElement } = e.target;
+  const grandParentElement = e.target.parentElement.parentElement;
+  const inputValue = e.target.value.replace(",", ".");
+
+  if (
+    !parentElement.classList.contains("error") &&
+    (isNaN(inputValue) || Number(inputValue) <= 0)
+  ) {
+    //<p class="text-red-500" >Insira um valor numérico maior que zero</p>
+    const errorTextElement = document.createElement("p");
+    errorTextElement.classList.add("text-red-500");
+    errorTextElement.innerText = "Insira um valor numérico maior que zero";
+
+    parentElement.classList.add("error");
+    grandParentElement.appendChild(errorTextElement);
+  } else if (
+    parentElement.classList.contains("error") &&
+    !isNaN(inputValue) &&
+    Number(inputValue) > 0
+  ) {
+    parentElement.classList.remove("error");
+    grandParentElement.querySelector("p").remove();
+  }
+}
+
+for (const formElement of form) {
+  if (formElement.tagName === "INPUT" && formElement.hasAttribute("name")) {
+    formElement.addEventListener("blur", validateInput);
+  }
+}
+
 //form.addEventListener("submit", renderProgression);
 form.addEventListener("submit", renderProgression);
+clearFormButton.addEventListener("click", clearForm);
